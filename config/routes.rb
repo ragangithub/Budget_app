eb8da0cd5ddae80e72715d129ab2214d272caa49
ragan <raganalemu3@gmail.com>
2023-09-13 12:32:06 +0300

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  devise_scope :user do
+    authenticated :user do
+      root to: 'categories#index', as: :authenticated_root
+    end
+
+    
+  end
 
   resources :categories, only: %i[index new create destroy] do
     resources :payments, only: %i[index new create]
