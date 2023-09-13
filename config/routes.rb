ce81375eab4745f7283e992ea7b5fb338c981401
ragan <raganalemu3@gmail.com>
2023-09-13 12:56:22 +0300

    Fix route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   
   devise_scope :user do
     authenticated :user do
       root to: 'categories#index', as: :authenticated_root
-
-      unauthenticated :user do
-        root to: 'splash#splash', as: :unauthenticated_root
-      end
     end
-end
+
+    unauthenticated :user do
+      root to: 'splash#splash', as: :unauthenticated_root
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # root to: 'categories#index'
 
   resources :categories, only: %i[index new create destroy] do
     resources :payments, only: %i[index new create]
